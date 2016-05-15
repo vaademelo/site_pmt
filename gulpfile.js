4720eb6cd3742d1763d70babc6d1b0b35faf5da9
Vinicius Adaime Alves de Melo <vaademelo@gmail.com>
2016-05-15 20:14:35 -0300

    gulp setup to jade and sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,27 @@
 var gulp = require('gulp'),
-  sass = require('gulp-ruby-sass');
+  sass = require('gulp-ruby-sass'),
+  autoprefixer = require("gulp-autoprefixer"),
+  minifycss = require("gulp-minify-css"),
+  rename = require("gulp-rename"),
+  ext_replace = require("gulp-ext-replace"),
+  jade = require("gulp-jade");
+ 
+gulp.task("templates", function(){
+  
+  var YOUR_LOCALS = {};
+  
+  gulp.src("app/**/index.jade")
+    .pipe(jade({
+      locals: YOUR_LOCALS
+    }))
+    .pipe(gulp.dest('../deploy/'));
+});
     
 gulp.task('sass', function(){
-  sass('app/**/*.sass')
+  sass('app/assets/stylesheets/*.sass')
       .on('error', sass.logError)
-      .pipe(gulp.dest('deploy/assets/css'));
+      .pipe(autoprefixer("last 2 version"))
+      .pipe(minifycss())
+      .pipe(rename({ suffix: ".min" }))
+      .pipe(gulp.dest('../deploy/assets/css/'));
 });
